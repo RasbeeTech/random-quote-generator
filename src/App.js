550da4983bf1b92513059495d7ca0456a2f95531
MykalB <mykalb081@gmail.com>
2021-06-02 17:12:18 -0400

    Update App.js
    - Basic formatting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='quote-box' className='Title'>
+      <h1>Random Quote Generator</h1>
+      <DisplayQuote />
+      <button id='new-quote'>Generate</button>
+      <a id='tweet-quote'>Tweet-quote</a>
     </div>
   );
 }
+
+ class DisplayQuote extends React.Component{
+   constructor(props){
+    super(props);
+    this.state = {};
+   }
+   render() {
+    return(
+      <div>
+        <p id='text'>text</p>
+        <p id='author'>author</p>
+      </div>
+    );
+   }
+ }
 
 export default App;
