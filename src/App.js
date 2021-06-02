6e24463b63577e012d9365e3d69b5677eff622c0
MykalB <mykalb081@gmail.com>
2021-06-02 17:31:22 -0400

    Update App.js
    - Refactor code

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function App() {
-  return (
-    <div id='quote-box' className='Title'>
+const QUOTE = {
+  text: '',
+  author: ''
+};
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+      auther: ''
+    };
+  }
+  render() {
+    return (
+    <div id='quote-box'>
       <h1>Random Quote Generator</h1>
       <DisplayQuote />
       <button id='new-quote'>Generate</button>
-      <a id='tweet-quote'>Tweet-quote</a>
+      <br />
+      <a id='tweet-quote' href='twitter.com/intent/tweet' target='_blank'>Tweet-quote</a>
+    </div>
+    );
+  }
+}
+
+function DisplayQuote(){
+  return(
+    <div id='quote'>
+      <p id='text'>text</p>
+      <p id='author'>author</p>
     </div>
   );
 }
-
- class DisplayQuote extends React.Component{
-   constructor(props){
-    super(props);
-    this.state = {};
-   }
-   render() {
-    return(
-      <div>
-        <p id='text'>text</p>
-        <p id='author'>author</p>
-      </div>
-    );
-   }
- }
 
 export default App;
