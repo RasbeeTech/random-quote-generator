ace9bfaffc47edf019ef447727d33ff41eaa2479
MykalB <mykalb081@gmail.com>
2021-06-03 11:53:45 -0400

    Update App.js
    - Refactor code
    - Code cleanup
    - implement api call for quotes

--- src/App.js
+++ src/App.js
@@ -2,46 +2,52 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-let currentQuote = {
-  text: '',
-  author: ''
-};
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.quotesObj = {};
     this.state = {
       text: '',
-      auther: ''
+      author: ''
     };
-    this.generateQuote = this.generateQuote.bind(this);
+    this.newQuote = this.newQuote.bind(this);
   }
-  generateQuote(){
-    this.setState({
-      text: 'Hello',
-      auther: '-Me'
+
+  componentWillMount() {
+    // Load quotes into object variable and set state.
+    fetch("https://type.fit/api/quotes")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      this.quotesObj=data;
+    }).then(()=>{
+      this.newQuote();
     });
   }
+
+  newQuote(){
+    // Generate random quote and set state.
+    let randomIndex = Math.floor(Math.random() * this.quotesObj.length);
+    let newQuote = this.quotesObj[randomIndex];
+    this.setState({
+      text: newQuote.text,
+      author: newQuote.author
+    });
+  }
+
   render() {
     return (
-    <div id='quote-box' className='container center'>
+    <div id='quote-box'>
       <h1>Random Quote Generator</h1>
-      <DisplayQuote text={this.state.text} auther={this.state.auther}/>
-      <button id='new-quote' onClick={this.generateQuote}>Generate</button>
+      <p id='text'>{this.state.text}</p>
+      <p id='author'>- {this.state.author}</p>
+      <button id='new-quote' onClick={this.newQuote}>Generate</button>
       <br />
       <a id='tweet-quote' href='twitter.com/intent/tweet' target='_blank'>Tweet-quote</a>
     </div>
     );
   }
 }
-
-function DisplayQuote(props){
-  return(
-    <div id='quote'>
-      <p id='text'>{props.text}</p>
-      <p id='author'>{props.auther}</p>
-    </div>
-  );
-}
-
 export default App;
