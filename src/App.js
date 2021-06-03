134ebfa5e8ebe437145913b4114052c62a62933a
MykalB <mykalb081@gmail.com>
2021-06-03 17:57:03 -0400

    Styling/jQuery
    - Additional styling
    - Added HEX colour pairs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
 import React from 'react';
+import $ from 'jquery';
 
+const colors = [
+  ['#FDB5AD','#FF6A5B'],
+  ['#FFEFBE','#FFD85B'],
+  ['#F2FFBE','#D2FB32'],
+  ['#C6FEB0','#6BFB32'],
+  ['#B0FEBE','#22FF48'],
+  ['#ABFFE4','#22FFB9'],
+  ['#ABF9FF','#34F2FF'],
+  ['#A2CDFF','#3492FF'],
+  ['#A2A8FF','#424FFF'],
+  ['#CB9AFB','#A142FF'],
+  ['#F19AFB','#EC3DFF'],
+  ['#FF98DA','#FF3DBA'],
+  ['#FF98B5','#FC3069'],
+  ['#BA7272','#B72323'],
+];
 class App extends React.Component {
   // A class to handle the view.
   constructor(props){
@@ -40,7 +57,7 @@ class App extends React.Component {
   render() {
     // Render view.
     return (
-    <div id='quote-box' className='card bg-warning'>
+    <div id='quote-box' className='card'>
       <div className='card-header'>
         <h2><b>Quote Generator</b></h2>
       </div>
@@ -59,10 +76,17 @@ class App extends React.Component {
         <a id='tweet-quote'href='twitter.com/intent/tweet' className='float-left' target='_blank'>
           <i class="fa fa-3x fa-twitter" />
         </a>
-        <button id='new-quote' className='btn btn-success float-right' onClick={this.newQuote}>Generate</button>
+        <button id='new-quote' className='btn float-right' onClick={this.newQuote}>Generate</button>
       </div>
     </div>
     );
   }
+}
+
+function changeColors(){
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  $('html body').css('background-color', color[1]);
+  $('#quote-box').css('background-color', color[0]);
+  $('#new-quote').css('background-color', color[1]);
 }
 export default App;
